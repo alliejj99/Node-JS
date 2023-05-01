2578d754e3cc14c14a4aad9be7d23c0c026112e5
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-05-01 19:53:09 +0900

    모듈 내보내기 다른 방법 알아보기

---
+++ 04. 모듈 내보내기 다른 방법/exports.js
@@ -0,0 +1,10 @@
+module.exports.A = 1;
+module.exports = function encrypt(data) {
+  return "encrypted data";
+};
+exports.encrypt = function encrypt(data) {
+  return "encrypted data";
+};
+
+// default 설정
+module.export = function encrypt(data) {};
