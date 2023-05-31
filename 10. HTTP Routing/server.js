cc48c378173e388a3d7b04ce5a3a55d6b49ec449
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-05-31 19:11:45 +0900

    HTTP Routing

---
+++ 10. HTTP Routing/server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        a: "a",
+        b: "b",
+      })
+    );
+  } else if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>About Page</h1>");
+    res.write("</body>");
+    res.write("</html>");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
